363601a2f20b8e3f3dcd36990940d9b55532515a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-19 18:25:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-     url = f'https://{URL_SHORTENR_WEBSITE}/api'
+     url = f'https://atglinks.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
