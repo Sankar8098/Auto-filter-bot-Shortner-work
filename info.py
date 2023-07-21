1fa0107b9088ede5e0ebcead1a8f6901c44759dd
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-21 22:41:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'LazyPrincess')
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23990433'))
 API_HASH = environ.get('API_HASH', 'e6c4b6ee1933711bc4da9d7d17e1eb20')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6237026610:AAHhvQbM1nH4bDCAw80Dmx9rCmeGBbMdmQg")
