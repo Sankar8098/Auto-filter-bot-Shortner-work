9a5a975edcfc3ce3c2f1cc84a4dc99b766847de8
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-28 09:36:14 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -33,7 +33,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='OkDa',
+                           switch_pm_text='okDa',
                            switch_pm_parameter="subscribe")
         return
 
